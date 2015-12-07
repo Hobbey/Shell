b3bc70c4704b23f5dd3b029919046957d19fb1d2
Hobbey <hobbey12@gmail.com>
2015-12-07 18:43:14 +0800

    统一风格

--- disk_check.sh
+++ disk_check.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 phone_num="XXX"
-host_ip=$(cat /etc/sysconfig/network-scripts/ifcfg-* | grep IPADDR | grep -v 127.0.0.1 | cut -d "=" -f 2 |  head -n 1 | cut -c 1-15)
 
-disk_used=0
-for i in $(df -hP | tail -n +2 | grep -v tmpfs | grep -v boot | awk '{print $5}' | sed 's/%//g'); do
-    if [[ $i -gt disk_used ]]; then
-        disk_used=$i
-    fi
-done
+disk_check () {
+    ##### disk_check_disk_used
+    ### disk_check_output_a,disk_check_output_b
+    
+    #####
+    disk_check_disk_used=0
+    for i in $(df -hP | tail -n +2 | grep -v tmpfs | grep -v boot | awk '{print $5}' | sed 's/%//g'); do
+        if [[ $i -gt disk_check_disk_used ]]; then
+            disk_check_disk_used=$i
+        fi
+    done
+    ###
+    disk_check_output_a="Disk_Space_Used:${disk_check_disk_used}"
+    
+    #####
+    host_ip=$(cat /etc/sysconfig/network-scripts/ifcfg-* | grep IPADDR | grep -v 127.0.0.1 | cut -d "=" -f 2 |  head -n 1 | cut -c 1-15)
+    ###
+    disk_check_output_b="${HOSTNAME}:${host_ip}"
+    
+#    echo -e "${disk_check_output_a}\n${disk_check_output_b}"
+}
 
 case $1 in
     -t)
-        echo "Get server ip:${host_ip}"
+        disk_check
         echo "Phone:${phone_num}"
-        echo "Message:${HOSTNAME}:${host_ip},disk_used:${disk_used}"
+        echo "${disk_check_output_b}"
+        echo "${disk_check_output_a}"
         ;;
-
+    
     *)
-        if [[ disk_used -ge 90 ]]; then
-            wget --post-data "phone=${phone_num}&content= ${HOSTNAME}:${host_ip},disk_used:${disk_used} &ac=send" http://sms-url
+        disk_check
+        if [[ disk_check_disk_used -ge 90 ]]; then
+            wget --post-data "phone=${phone_num}&content=${disk_check_output_b},${disk_check_output_a}&ac=send" sms-url
         fi
         ;;
 esac
@@ -27,4 +43,3 @@ esac
 vim /home/script/disk_check.sh
 chmod 700 /home/script/disk_check.sh
 0 9 * * * /bin/bash /home/script/disk_check.sh
-
