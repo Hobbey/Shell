f21a59a901b267885a1213ce02329b480498b5da
Hobbey <hobbey12@gmail.com>
2015-12-08 11:05:52 +0800

    add

---
+++ Shell 常用功能组件/1_判断输入日期的合法性.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# cal day month year
+if [[ $1 =~ ^[0-9]{8}$ ]]; then
+    if [[ $(cal ${1:6:2} ${1:4:2} ${1:0:4}) ]]; then
+        echo Ok
+    else
+        2>/dev/null
+        echo Error
+    fi
+else
+    echo "Usage:yyyymmdd"
+    exit 1
+fi
+
+
+
+TEST:
+
+[root@cloud01 script]# ./test.sh 20160208
+OK
+
+[root@cloud01 script]# ./test.sh 20160232
+Error
+
+[root@cloud01 script]# ./test.sh 2016023223
+Usage:yyyymmdd
+
+[root@cloud01 script]# ./test.sh fafafwaf
+Usage:yyyymmdd
