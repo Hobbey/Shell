d51ac8775a0c3d5240e84109aaa18920389b9426
Hobbey <hobbey12@gmail.com>
2015-12-15 17:11:05 +0800

    add

---
+++ check_ping.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ip_prefix="192.168.12."
+ip_postfix="39 40 55 85"
+ping_num=4
+
+alarm=0
+num=0 #出错机器数
+for i in $ip_postfix; do
+#    packet_loss=75 # test
+    packet_loss=$(ping ${ip_prefix}${i} -c ${ping_num} | grep "packet loss" | cut -d "%" -f 1 | awk '{print $NF}')
+    if [[ packet_loss -gt 25 ]]; then #丢包 25% 以上 
+        alarm=1
+        check_ping_ip[num]="${ip_prefix}${i}"
+        check_ping_lost[num]="${packet_loss}"
+        (( ++num ))
+    fi
+done
+(( --num ))
+
+for j in $(seq 0 $num ); do
+    echo  "${check_ping_ip[j]}: ${check_ping_lost[j]}% packet loss"
+done
